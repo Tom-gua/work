a64f4fb69e46d214ef76f1d632e6daa3786cb55b
tom <526019128@qq.com>
2018-07-12 00:01:17 +0800

    修改

--- static/dist/js/base.js
+++ static/dist/js/base.js
@@ -4,6 +4,6 @@
 var __main = function() {
     var d = new Date()
     var year = d.getFullYear()
-    $("#date-times")[0].innerHTML = `&copy; ${year} 北京弋游科技有限公司, Inc.`
+    $("#date-times")[0].innerHTML = `&copy; ${year} 北京奇点科技有限公司, Inc.`
 }
 __main()
