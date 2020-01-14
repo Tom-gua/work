a66cd7a27b300b7cd804c04f1459390393a0b78c
Tom Li <tom.li@klook.com>
2020-01-14 17:02:00 +0800

    修改为弋游文案

--- static/dist/js/base.js
+++ static/dist/js/base.js
@@ -1,9 +1,6 @@
-/**
- * Created by liteng on 2017/6/17.
- */
 var __main = function() {
     var d = new Date()
     var year = d.getFullYear()
-    $("#date-times")[0].innerHTML = `&copy; ${year} 北京奇点科技有限公司, Inc.`
+    $("#date-times")[0].innerHTML = `&copy; ${year} 北京弋游科技有限公司, Inc.`
 }
 __main()
